603f6bd3e3e7570edd4850d51219bc585d2851c2
Antonio <Antonio@DESKTOP-CEIAAKE>
2023-07-23 16:30:57 -0300

    zfzdf

--- src/br/com/fiap/banco/Test.java
+++ src/br/com/fiap/banco/Test.java
@@ -11,19 +11,19 @@ public class Test {
 	public static void main (String[] args){
 		
 		Conta cc = new Conta ();
-		cc.saldo = 50.0;
-		cc.agencia = 123;
-		cc.numero = 321;
+		cc.depositar (50.0);
+		cc.setAgencia (123);
+		cc.setNumero (321);
 		
 		cc.depositar(1000);
 		
 		
-		System.out.println(cc.verificarSaldo());
+		System.out.println( cc.getrSaldo() );
 		
 		Conta poupanca = new Conta(111, 222, 1000);
 		poupanca.retirar(50);
 		
-		System.out.println(poupanca.verificarSaldo());
+		System.out.println( poupanca.getrSaldo() );
 		
 	
 		// java doc = @author @deprecated @param @return @see @since @exception @throws @version 
